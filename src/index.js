cfa5324ed4b5f321202b6d8969d278154ac1a8ad
kopychev <v.kopychev@alekongroup.ru>
2020-07-18 09:26:19 +0100

    RPG game tile view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import rootReducer from './reducers'
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
